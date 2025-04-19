85cdb3927bab39ed8c6d4d6b270d752f034050a4
sarah <moousarah@gmail.com>
2025-04-19 01:29:30 +0100

    Update instances_creation.sql

--- sql code/instances_creation.sql
+++ sql code/instances_creation.sql
@@ -43,8 +43,8 @@ INSERT INTO StationTab VALUES (
     StationPrincipaleType(
         'SP001', 'Haï El Badr', 3.080000, 36.750000,
         TabMoyenTransport(
-            (SELECT VALUE(m) FROM MoyenTransportTab m WHERE m.code_transport = 'M'),
-            (SELECT VALUE(m) FROM MoyenTransportTab m WHERE m.code_transport = 'B')
+            (SELECT REF(m) FROM MoyenTransportTab m WHERE m.code_transport = 'M'),
+            (SELECT REF(m) FROM MoyenTransportTab m WHERE m.code_transport = 'B')
         )
     )
 );
@@ -54,8 +54,8 @@ INSERT INTO StationTab VALUES (
     StationPrincipaleType(
         'SP002', 'El Harrach Centre', 3.100000, 36.760000,
         TabMoyenTransport(
-            (SELECT VALUE(m) FROM MoyenTransportTab m WHERE m.code_transport = 'M'),
-            (SELECT VALUE(m) FROM MoyenTransportTab m WHERE m.code_transport = 'TR')
+            (SELECT REF(m) FROM MoyenTransportTab m WHERE m.code_transport = 'M'),
+            (SELECT REF(m) FROM MoyenTransportTab m WHERE m.code_transport = 'TR')
         )
     )
 );
@@ -65,8 +65,8 @@ INSERT INTO StationTab VALUES (
     StationPrincipaleType(
         'SP003', 'Place des Martyrs', 3.060000, 36.780000,
         TabMoyenTransport(
-            (SELECT VALUE(m) FROM MoyenTransportTab m WHERE m.code_transport = 'M'),
-            (SELECT VALUE(m) FROM MoyenTransportTab m WHERE m.code_transport = 'TR')
+            (SELECT REF(m) FROM MoyenTransportTab m WHERE m.code_transport = 'M'),
+            (SELECT REF(m) FROM MoyenTransportTab m WHERE m.code_transport = 'TR')
         )
     )
 );
@@ -76,18 +76,19 @@ INSERT INTO StationTab VALUES (
     StationPrincipaleType(
         'SP004', 'Khelifa Boukhalfa', 3.060500, 36.775500,
         TabMoyenTransport(
-            (SELECT VALUE(m) FROM MoyenTransportTab m WHERE m.code_transport = 'B'),
-            (SELECT VALUE(m) FROM MoyenTransportTab m WHERE m.code_transport = 'M')
+            (SELECT REF(m) FROM MoyenTransportTab m WHERE m.code_transport = 'B'),
+            (SELECT REF(m) FROM MoyenTransportTab m WHERE m.code_transport = 'M')
         )
     )
 );
+
 
 
 -- Station Secondaire 1
 INSERT INTO StationTab VALUES (
     StationSecondaireType(
         'SS001', 'Les Fusillés', 3.090000, 36.755000,
-        (SELECT VALUE(m) FROM MoyenTransportTab m WHERE m.code_transport = 'MET')
+        (SELECT REF(m) FROM MoyenTransportTab m WHERE m.code_transport = 'M')
     )
 );
 
@@ -95,7 +96,7 @@ INSERT INTO StationTab VALUES (
 INSERT INTO StationTab VALUES (
     StationSecondaireType(
         'SS002', 'Agha', 3.105000, 36.765000,
-        (SELECT VALUE(m) FROM MoyenTransportTab m WHERE m.code_transport = 'TRN')
+        (SELECT REF(m) FROM MoyenTransportTab m WHERE m.code_transport = 'TR')
     )
 );
 
@@ -103,7 +104,7 @@ INSERT INTO StationTab VALUES (
 INSERT INTO StationTab VALUES (
     StationSecondaireType(
         'SS003', 'Bab Ezzouar', 3.170000, 36.710000,
-        (SELECT VALUE(m) FROM MoyenTransportTab m WHERE m.code_transport = 'TRN')
+        (SELECT REF(m) FROM MoyenTransportTab m WHERE m.code_transport = 'TR')
     )
 );
 
@@ -111,7 +112,7 @@ INSERT INTO StationTab VALUES (
 INSERT INTO StationTab VALUES (
     StationSecondaireType(
         'SS004', 'Bir Mourad Rais', 3.050000, 36.760000,
-        (SELECT VALUE(m) FROM MoyenTransportTab m WHERE m.code_transport = 'BUS')
+        (SELECT REF(m) FROM MoyenTransportTab m WHERE m.code_transport = 'B')
     )
 );
 
@@ -119,7 +120,7 @@ INSERT INTO StationTab VALUES (
 INSERT INTO StationTab VALUES (
     StationSecondaireType(
         'SS005', 'Ain Naadja', 3.060000, 36.745000,
-        (SELECT VALUE(m) FROM MoyenTransportTab m WHERE m.code_transport = 'BUS')
+        (SELECT REF(m) FROM MoyenTransportTab m WHERE m.code_transport = 'B')
     )
 );
 
@@ -127,9 +128,11 @@ INSERT INTO StationTab VALUES (
 INSERT INTO StationTab VALUES (
     StationSecondaireType(
         'SS006', 'Sidi Bel Abbès', 3.180000, 36.690000,
-        (SELECT VALUE(m) FROM MoyenTransportTab m WHERE m.code_transport = 'TRN')
+        (SELECT REF(m) FROM MoyenTransportTab m WHERE m.code_transport = 'TN')
     )
 );
+
+
 
 
 
@@ -182,6 +185,90 @@ INSERT INTO LigneTab VALUES(
     )
 );
 
+-- insertion des tronçons
+-- Tronçons pour le Métro
+INSERT INTO TronconTab VALUES (
+    TronconType(101,
+        (SELECT REF(s) FROM StationTab s WHERE s.code_station = 'SP001'),  -- Haï El Badr
+        (SELECT REF(s) FROM StationTab s WHERE s.code_station = 'SS001'),  -- Les Fusillés
+        1.50,
+        (SELECT REF(l) FROM LigneTab l WHERE l.code_ligne = 'M001'),
+        NULL
+    )
+);
+
+INSERT INTO TronconTab VALUES (
+    TronconType(102,
+        (SELECT REF(s) FROM StationTab s WHERE s.code_station = 'SS001'),  -- Les Fusillés
+        (SELECT REF(s) FROM StationTab s WHERE s.code_station = 'SP002'),  -- El Harrach Centre
+        1.80,
+        (SELECT REF(l) FROM LigneTab l WHERE l.code_ligne = 'M001'),
+        NULL
+    )
+);
+
+INSERT INTO TronconTab VALUES (
+    TronconType(103,
+        (SELECT REF(s) FROM StationTab s WHERE s.code_station = 'SP004'),  -- Khelifa Boukhalfa
+        (SELECT REF(s) FROM StationTab s WHERE s.code_station = 'SP003'),  -- Place des Martyrs
+        1.30,
+        (SELECT REF(l) FROM LigneTab l WHERE l.code_ligne = 'M001'),
+        NULL
+    )
+);
+
+-- Tronçons pour le Tramway
+INSERT INTO TronconTab VALUES (
+    TronconType(201,
+        (SELECT REF(s) FROM StationTab s WHERE s.code_station = 'SP002'),  -- El Harrach Centre
+        (SELECT REF(s) FROM StationTab s WHERE s.code_station = 'SS002'),  -- Agha
+        2.20,
+        (SELECT REF(l) FROM LigneTab l WHERE l.code_ligne = 'TR001'),
+        NULL
+    )
+);
+
+INSERT INTO TronconTab VALUES (
+    TronconType(202,
+        (SELECT REF(s) FROM StationTab s WHERE s.code_station = 'SS002'),  -- Agha
+        (SELECT REF(s) FROM StationTab s WHERE s.code_station = 'SS003'),  -- Bab Ezzouar
+        3.10,
+        (SELECT REF(l) FROM LigneTab l WHERE l.code_ligne = 'TR001'),
+        NULL
+    )
+);
+
+-- Tronçons pour le Bus
+INSERT INTO TronconTab VALUES (
+    TronconType(301,
+        (SELECT REF(s) FROM StationTab s WHERE s.code_station = 'SP004'),  -- Khelifa Boukhalfa
+        (SELECT REF(s) FROM StationTab s WHERE s.code_station = 'SS004'),  -- Bir Mourad Rais
+        2.00,
+        (SELECT REF(l) FROM LigneTab l WHERE l.code_ligne = 'B001'),
+        NULL
+    )
+);
+
+INSERT INTO TronconTab VALUES (
+    TronconType(302,
+        (SELECT REF(s) FROM StationTab s WHERE s.code_station = 'SS004'),  -- Bir Mourad Rais
+        (SELECT REF(s) FROM StationTab s WHERE s.code_station = 'SS005'),  -- Ain Naadja
+        2.20,
+        (SELECT REF(l) FROM LigneTab l WHERE l.code_ligne = 'B001'),
+        NULL
+    )
+);
+
+-- Tronçons pour le Train
+INSERT INTO TronconTab VALUES (
+    TronconType(401,
+        (SELECT REF(s) FROM StationTab s WHERE s.code_station = 'SS003'),  -- Bab Ezzouar
+        (SELECT REF(s) FROM StationTab s WHERE s.code_station = 'SS006'),  -- Sidi Bel Abbès
+        6.50,
+        (SELECT REF(l) FROM LigneTab l WHERE l.code_ligne = 'TN001'),
+        NULL
+    )
+);
 
 
 -- Insertion des navettes pour les lignes
@@ -290,4 +377,82 @@ INSERT INTO VoyageTab VALUES
 INSERT INTO VoyageTab VALUES
 ('V0014', TO_DATE('2025-01-07 08:00:00', 'YYYY-MM-DD HH24:MI:SS'), 30, TO_DATE('2025-01-07 08:00:00', 'YYYY-MM-DD HH24:MI:SS'), 'Retour', 50, 'Accident', (SELECT REF(n) FROM NavetteTab n WHERE n.num_navette = 'N003'));
 
+-- updates of table ligne : because of station codes
+UPDATE LigneTab l
+SET l.station_depart = (SELECT REF(s) FROM StationTab s WHERE s.code_station = 'SP001'),
+    l.station_arrivee = (SELECT REF(s) FROM StationTab s WHERE s.code_station = 'SS001'),
+    l.liste_navettes = CAST(
+        MULTISET(
+            SELECT REF(n) FROM NavetteTab n WHERE n.num_navette IN ('N003', 'N004')
+        ) AS TabNavette
+    ),
+    l.liste_troncons = CAST(
+        MULTISET(
+            SELECT REF(t) FROM TronconTab t WHERE t.num_troncon IN (101, 102, 103)
+        ) AS TabTroncon
+    )
+WHERE l.code_ligne = 'M001';
 
+UPDATE LigneTab l
+SET l.station_depart = (SELECT REF(s) FROM StationTab s WHERE s.code_station = 'SP004'),
+    l.station_arrivee = (SELECT REF(s) FROM StationTab s WHERE s.code_station = 'SS004'),
+    l.liste_navettes = CAST(
+        MULTISET(
+            SELECT REF(n) FROM NavetteTab n WHERE n.num_navette IN ('N001', 'N002')
+        ) AS TabNavette
+    ),
+    l.liste_troncons = CAST(
+        MULTISET(
+            SELECT REF(t) FROM TronconTab t WHERE t.num_troncon IN (301, 302)
+        ) AS TabTroncon
+    )
+WHERE l.code_ligne = 'B001';
+
+
+UPDATE LigneTab l
+SET l.station_depart = (SELECT REF(s) FROM StationTab s WHERE s.code_station = 'SP002'),
+    l.station_arrivee = (SELECT REF(s) FROM StationTab s WHERE s.code_station = 'SS003'),
+    l.liste_navettes = CAST(
+        MULTISET(
+            SELECT REF(n) FROM NavetteTab n WHERE n.num_navette IN ('N005', 'N006')
+        ) AS TabNavette
+    ),
+    l.liste_troncons = CAST(
+        MULTISET(
+            SELECT REF(t) FROM TronconTab t WHERE t.num_troncon IN (201, 202)
+        ) AS TabTroncon
+    )
+WHERE l.code_ligne = 'TR001';
+
+UPDATE LigneTab l
+SET l.station_depart = (SELECT REF(s) FROM StationTab s WHERE s.code_station = 'SS003'),
+    l.station_arrivee = (SELECT REF(s) FROM StationTab s WHERE s.code_station = 'SS006'),
+    l.liste_navettes = CAST(
+        MULTISET(
+            SELECT REF(n) FROM NavetteTab n WHERE n.num_navette IN ('N007', 'N008')
+        ) AS TabNavette
+    ),
+    l.liste_troncons = CAST(
+        MULTISET(
+            SELECT REF(t) FROM TronconTab t WHERE t.num_troncon IN (401)
+        ) AS TabTroncon
+    )
+WHERE l.code_ligne = 'TN001';
+
+-- update navette to add liste voyages :
+BEGIN
+  FOR rec IN (SELECT num_navette FROM NavetteTab) LOOP
+    UPDATE NavetteTab n
+    SET n.liste_voyages = CAST(
+      MULTISET(
+        SELECT REF(v)
+        FROM VoyageTab v
+        WHERE v.navette = REF(n)
+      ) AS TabVoyage
+    )
+    WHERE n.num_navette = rec.num_navette;
+  END LOOP;
+END;
+/
+-- verification
+select * from NavetteTab;
